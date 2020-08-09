d3d7a31b84876789d72444bd5c2d16a1fa205885
Dean James <xurion@hotmail.com>
2020-08-09 18:56:36 +0100

    Stripped blacklist function for now. Updated readme.

--- MountRoulette.lua
+++ MountRoulette.lua
@@ -3,12 +3,8 @@ _addon.author  = 'Dean James (Xurion of Bismarck)'
 _addon.version = '3.0.0'
 _addon.commands = {'mountroulette', 'mr'}
 
-require('luau')
+require('lists')
 resources = require('resources')
-config = require('config')
-settings = config.load({
-    blacklist = S{}
-})
 
 math.randomseed(os.time())
 
@@ -29,8 +25,8 @@ function update_allowed_mounts()
             end)
             local mount = possible_mounts[mount_index]
 
-            -- Add this to allowed mounts if it is not already there and it is not blacklisted
-            if not allowed_mounts:contains(mount) and not settings.blacklist:contains(mount) then
+            -- Add this to allowed mounts if it is not already there
+            if not allowed_mounts:contains(mount) then
                 allowed_mounts:append(mount)
             end
         end
@@ -42,7 +38,7 @@ windower.register_event('load', function()
 end)
 
 windower.register_event('incoming chunk', function(id)
-    if id == 0x055 then
+    if id == 0x055 then --ki update
         update_allowed_mounts()
     end
 end)
@@ -51,7 +47,7 @@ windower.register_event('addon command', function(command, ...)
     command = command and command:lower() or 'mount'
 
     if commands[command] then
-        commands[command](L{...})
+        commands[command]()
     else
         commands.help()
     end
@@ -70,56 +66,14 @@ commands.mount = function()
         end
     end
 
-    if #allowed_mounts == 0 then
-        windower.add_to_chat(8, 'You have blacklisted all mounts')
-        return
-    end
-
     -- Generate random number and use it to choose a mount
     local mount_index = math.ceil(math.random() * #allowed_mounts)
     windower.send_command('input /mount ' .. allowed_mounts[mount_index])
-end
-
-commands.blacklist = function(args)
-    local operation = args:remove(1)
-    local mount = args:concat(' '):lower()
-
-    if not operation or not mount then
-        commands.help()
-        return
-    end
-
-    if not possible_mounts:contains(mount) then
-        windower.add_to_chat(8, 'Unknown mount ' .. mount)
-        return
-    end
-
-    if operation == 'add' and not settings.blacklist:contains(mount) then
-        for k, v in ipairs(allowed_mounts) do
-            if v == mount then
-                allowed_mounts:remove(k)
-            end
-        end
-        settings.blacklist:add(mount)
-        windower.add_to_chat(8, 'The ' .. mount .. ' mount is now blacklisted')
-        settings:save()
-    elseif operation == 'remove' then
-        for k, v in ipairs(T(settings.blacklist)) do
-            if v == mount then
-                settings.blacklist:remove(k)
-            end
-        end
-        allowed_mounts:append(mount)
-        windower.add_to_chat(8, 'The ' .. mount .. ' mount is no longer blacklisted')
-        settings:save()
-    end
 end
 
 commands.help = function()
     windower.add_to_chat(8, '---Mount Roulette---')
     windower.add_to_chat(8, 'Available commands:')
     windower.add_to_chat(8, '//mr mount (or just //mr) - Selects a mount at random, or dismounts if mounted')
-    windower.add_to_chat(8, '//mr blacklist add <mount> - blacklist a mount so it is never randomly selected')
-    windower.add_to_chat(8, '//mr blacklist remove <mount> - remove a blacklisted mount')
     windower.add_to_chat(8, '//mr help - displays this help')
 end
--- README.md
+++ README.md
@@ -11,11 +11,7 @@ A Lua addon to summon a mount at random for Windower 4. Mimics the FFXIV Mount R
 
 `//mr`
 
-### Blacklist mounts
-
-You can blacklist certain mounts so Mount Roulette will never choose them.
-
-`//mr blacklist raptor`
+This will also dismount you if you're currently mounted.
 
 ## Mount music
 In an earlier version, this addon disabled mount music. This is now removed in favour of the MountMuzzle addon, available via Windower.
